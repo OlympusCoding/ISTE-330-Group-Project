73fadd9a9bc81de728113f7dc3b1306e22af3253
Flavio Medina (RIT Student) <fjm8881@rit.edu>
2025-04-09 14:46:33 -0400

    Completed some tasks but will finish it this week

--- db/insertDatabase.sql
+++ db/insertDatabase.sql
@@ -7,19 +7,61 @@
 
 
 -- Users
-INSERT INTO users (username, password, email) VALUES ('', '', '');
+INSERT INTO 
+    users (username, password, email) 
+-- Contains 7 students & Auto Increment starts at 100
+VALUES 
+    -- userID: 100
+    ('flaviomedinajr', 'student1', 'fjm8881@rit.edu'),  
+
+    -- userID: 101 
+    ('charlescoleman', 'student2', 'cxctis@rit.edu'),   
+
+    -- userID: 102
+    ('seanguyon', 'student3', 'spg5070@rit.edu'),    
+
+    -- userID: 103     
+    ('willjacobs', 'student4', 'whj3443@rit.edu'),    
+
+    -- userID: 104 
+    ('davidkalinowski', 'student5', 'dmk7090@rit.edu'), 
+
+    -- userID: 105
+    ('johndoe', 'student6', 'johndoe@rit.edu'),    
+
+    -- userID: 106 
+    ('tigersrit', 'student7', 'tigersrit@rit.edu');     
+
 
 
 -- Interests
-INSERT INTO interests (description) VALUES ('');
+INSERT INTO 
+    interests (description) 
+VALUES 
+    ('Software Engineering'), -- dse
+    ('Data Science'), -- dse
+    ('Studio Arts'), -- sa
+    ('Chemistry',) -- scms
+    ('Biochemistry'), -- scms
+    ('Accounting'), -- fad
+    ('Finance'); -- fad
 
 
 -- User Interests
-INSERT INTO user_interets (userID, interestID) VALUES (0);
+INSERT INTO 
+    user_interets (userID, interestID) 
+VALUES 
+    (50, 1);
 
 
 -- Department
-INSERT INTO departments (name) VALUES ('');
+INSERT INTO 
+    departments (name) 
+VALUES 
+    ('Finance and Accounting Department'), -- fad
+    ('Department of Software Engineering'), -- dse
+    ('School of Chemistry and Materials Science'), -- scms
+    ('School of Art'); -- sa
 
 
 -- Faculty
