2491e08017f049381e22ec60df14426540e7696d
OlympusCoding <olympuscoding@gmail.com>
2025-04-18 01:10:45 -0400

    Stubbed out methods for GUIFascade

--- src/backend/GUIFascade.java
+++ src/backend/GUIFascade.java
@@ -1,6 +1,9 @@
 package backend;
 
+import java.util.List;
+
 import types.user.UserParams;
+import types.user.userTypes.*;
 
 public class GUIFascade {
     
@@ -32,9 +35,102 @@ public class GUIFascade {
 
     public boolean Register(UserParams params)
     {
+        // Create New User with all sub types etc
         return true;
     }
 
+    public boolean ChangeUserFullname()
+    {
+        ChangeUserFirstName();
+        ChangeUserLastname();
+        return true;
+    }
+
+    public boolean ChangeUserFirstName()
+    {
+        return true;
+    }
+
+    public boolean ChangeUserLastname()
+    {
+        return true;
+    }
+
+    public boolean ChangePassword()
+    {
+        // Requires knowing old password to change password
+        return true;
+    }
+
+    public boolean AddFacultyAbstract()
+    {
+        // Adds a new abstract to a faculty member
+        return true;
+    }
+
+    public boolean UpdateFacultyAbstract()
+    {
+        // Updates an existing abstract of a faculty member(s)
+        return true;
+    }
+
+    public boolean RemoveFacultyAbstract()
+    {
+        // Removes a faculty member from an abstract
+        return true;
+    }
+
+    public boolean CreateFacultyInterest()
+    {
+        // Creates a new Faculty Interest
+        return true;
+    }
+
+    public boolean UpdateFacultyInterest()
+    {
+        // Updates an existing Faculty Interest
+        return true;
+    }
+
+    public boolean RemoveFacultyInterest()
+    {
+        // Removes Faculty Interest
+        return true;
+    }
+
+    public boolean CreateProject()
+    {
+        // Creates a new project
+        return true;
+    }
+
+    public boolean UpdateProject()
+    {
+        // Updates a Project
+        return true;
+    }
+
+    public boolean DeleteProject()
+    {
+        // Deletes a project
+        return true;
+    }
+
+    // Project Interests??
+
+    public List<Student> SearchForStudentByInterest()
+    {
+        // Searches for students based on an interest
+        // Returns a list of students that share that interest
+        return null;
+    }
+
+    public List<Faculty> SearchForFacultyByInterest()
+    {
+        // Searches for faculty based on an interest
+        // Returns a list of faculty that share that interest
+        return null;
+    }
 
 
 
